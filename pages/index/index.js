31dc167297c327c49875af5e22666d9960aea395
zkrzhangsq <zkrzhangsq@abchinalife.com>
2021-04-27 11:09:49 +0800

    联调开始

--- pages/index/index.js
+++ pages/index/index.js
@@ -47,7 +47,7 @@ Page({
       })
     } else {
       wx.request({
-        url: 'http://192.168.43.232:6001/zhixing/user/userLogin',
+        url: 'http://192.168.43.232:6001/zhixing/weChatLoginController/userLogin',
         method: 'post',
         data: {
           username: that.data.username,
